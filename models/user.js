8dfe8b4befa1996926ac99b65540ef4a0a3a4e15
Fransditoooo <fransdito.bayu.ft24@mail.umy.ac.id>
2026-07-27 15:13:45 +0700

    mendefinisikan Kolom Password User

--- models/user.js
+++ models/user.js
@@ -10,4 +10,10 @@ module.exports = (sequelize, DataTypes) => {
             allowNull: false,
             unique: true,
         },
-        
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        }
+    });
+    
+  
