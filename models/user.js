657f55250d45463671ff9e8c48dd1458a31922e0
Fransditoooo <fransdito.bayu.ft24@mail.umy.ac.id>
2026-07-27 15:12:36 +0700

    mendefinisikan Primary Key User

--- models/user.js
+++ models/user.js
@@ -1,3 +1,8 @@
 module.exports = (sequelize, DataTypes) => {
     const User = sequelize.define('User', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
         
