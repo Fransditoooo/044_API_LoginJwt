8ffbca3a02b80dc1e3c0820e58e39d8bb0aa56bf
Fransditoooo <fransdito.bayu.ft24@mail.umy.ac.id>
2026-07-27 15:09:00 +0700

    mendefinisikan Kolom Deskripsi Komik

--- models/komik.js
+++ models/komik.js
@@ -8,4 +8,7 @@ module.exports = (sequelize, DataTypes) => {
         title: {
             type: DataTypes.STRING,
         },
+        description: {
+            type: DataTypes.STRING,
+        },
        
