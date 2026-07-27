d829b8ebb61afdda099432ca82cae824cb902c4d
Fransditoooo <fransdito.bayu.ft24@mail.umy.ac.id>
2026-07-27 15:13:10 +0700

    mendefinisikan Kolom Email User

--- models/user.js
+++ models/user.js
@@ -5,4 +5,9 @@ module.exports = (sequelize, DataTypes) => {
             primaryKey: true,
             autoIncrement: true
         },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
         
